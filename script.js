a4ef2bd385a87c7c5dc0ab61f957b7bd80551601
Aubrey <ragandanaubrey@gmail.com>
2024-08-24 16:50:02 +0900

    Fix the function playRound

--- script.js
+++ script.js
@@ -1,28 +1,52 @@
-let humanScore = 0;
-let computerScore = 0;
+// Return one of the following string values
+const getComputerChoice = function () {
+  const choices = ["rock", "paper", "scissors"];
+  const randomNumber = Math.trunc(Math.random() * choices.length);
+  return choices[randomNumber];
+};
 
-// COMPUTER
-const getComputerChoice = function () {};
-
-// PLAYER
 const getHumanChoice = function () {
-  const playerChoice = prompt("Pick your choice (Rock Paper Scissors)?: ");
-  return playerChoice;
+  const player = prompt("Choose one (Rock, Paper, or Scissors): ");
+  return player;
 };
 
-// PLAY
+console.log(getHumanChoice());
+const humanScore = 0;
+const computerScore = 0;
+
 const playRound = function (humanChoice, computerChoice) {
-  if (humanChoice === "rock" && computerChoice === "rock") {
-    console.log("It's a tie")
-  } else if (humanChoice === "paper" && computerChoice === "paper") {
-    console.log("It's a tie")
-  } else if (humanChoice === "scissors" && computerChoice === "scissors") {
-    console.log("It's a tie")
-  } else if (humanChoice === "rock" )
+  if (humanChoice.toLowerCase() === "rock" && computerChoice === "scissors") {
+    console.log("You win! Rock beats Scissors");
+  } else if (
+    humanChoice.toLowerCase() === "rock" &&
+    computerChoice === "paper"
+  ) {
+    console.log("You lose! Paper beats Rock");
+  } else if (
+    humanChoice.toLowerCase() === "scissors" &&
+    computerChoice === "paper"
+  ) {
+    console.log("You win! Scissors beats Paper");
+  } else if (
+    humanChoice.toLowerCase() === "scissors" &&
+    computerChoice === "rock"
+  ) {
+    console.log("You lose! Rock beats Scissors");
+  } else if (
+    humanChoice.toLowerCase() === "paper" &&
+    computerChoice === "rock"
+  ) {
+    console.log("You win! Paper beats Rock");
+  } else if (
+    humanChoice.toLowerCase() === "paper" &&
+    computerChoice === "scissors"
+  ) {
+    console.log("You lose! Scissors beats Paper");
+  } else {
+    console.log("It's a tie");
+  }
 };
-
-// PLAY THE ENTIRE GAME
-const playGame = function () {};
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
+console.log(playRound(humanSelection, computerSelection));
